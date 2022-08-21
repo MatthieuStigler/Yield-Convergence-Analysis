514e0209ce4cbccb68c722e39d692e64ab3da05f
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2022-08-21 12:59:53 +0200

    Merge FAO16 ylds

--- code_R/0_8_yieldPot_assemble_rJcYdFg.R
+++ code_R/0_8_yieldPot_assemble_rJcYdFg.R
@@ -20,20 +20,27 @@ mat_list_dir("temp", pattern = "potential|ygap") %>%
   select(1:3) %>% 
   knitr::kable()
 
-##
+## read one raw
+yld_gap_irr_raw <- read_csv("dataRaw/GAEZ/ygap_rice_irr.csv", skip=10)
+
+## Read processed by Nathan
 yldpot_wheat <- read_dta("temp/wheat_yield_potential.dta")
 yldpot_maize <- read_dta("temp/maize_yield_potential.dta")
 yldpot_rice <- read_dta("temp/ygapGAEZ_rice.dta")
 yldpot_rice_raw <- read_dta("dataRaw/GAEZ/country_Calories_stats_web.dta") %>% 
   select(ISO_A3, post1500AverageCaloriesmean)
 
+## FAO codes
+FAO_codes <- read_csv("dataRaw/FAOSTAT_countrycodes.csv") 
 
-
+## FAOSTAT data
+FAO_2020_Y <- read_rds("data_intermediary/FAOSTAT_3crops_2020_Yonly_rSeWhYs.rds")
 
 ################################
 #'## Prepare data
 ################################
 
+## Bind all maize/wheat/rice
 yldpot_all <- yldpot_wheat %>%  
   select(country, countrycode, iso3, commodity, potent_rain_int) %>% 
   rbind(yldpot_maize %>% 
@@ -53,8 +60,41 @@ yldpot_all_l <- yldpot_all %>%
 yldpot_all_l
 
 ################################
+#'## Check iso
+################################
+
+## GAEZ raw? It is very hard to understand whether this includes China !! We will assume not...
+yld_gap_irr_raw %>% 
+  filter(str_detect(Country, "China|Taiwan"))
+
+## GAEZ gives for China = mainland + Taiwan!
+yldpot_all_l %>% 
+  filter(str_detect(country, "China|Taiwan"))
+
+## what are the possible codes?
+FAO_codes %>% 
+  filter(str_detect(Country, "China")) %>% 
+  print(n=10)
+
+### FAOSTAT gives for mainland (41) and Taiwan!
+FAO_2020_Y %>% 
+  distinct(countrycode, country, iso3) %>% 
+  filter(str_detect(country, "China")) %>% 
+  print(n=10)
+
+## SOLUTION: assign 41/CHN
+yldpot_all_l_c <- yldpot_all_l %>% 
+  mutate(countrycode=if_else(country=="China",
+                             41,countrycode),
+         iso3=if_else(country=="China",
+                             "CHN",iso3))
+
+yldpot_all_l_c %>% 
+  filter(str_detect(country, "China|Taiwan"))
+
+################################
 #'## Export data
 ################################
 
-write_dta(yldpot_all_l, "data_intermediary/yld_potential_rJcYdFg.dta")
+write_dta(yldpot_all_l_c, "data_intermediary/yld_potential_rJcYdFg.dta")
 ## READ AS: yld_pot <- read_dta("data_intermediary/yld_potential_rJcYdFg.dta")
---
+++ code_R/1_1_merge_FAOSTAT_yield_pot_rWsDaQp.R
@@ -0,0 +1,90 @@
+#' ---
+#' Title: "Merge yld, add lags etc"
+#' Author: "Matthieu"
+#' Date: 2022-08-20
+#' runMat: TRUE
+#' ---
+
+
+
+library(matPkg)
+library(haven)
+library(tidyverse)
+source("code_R/888_misc_functions.R")
+
+################################
+#'## Read data
+################################
+
+yld_smooth <- read_csv("temp/yield_pred_3crops.csv")
+FAO_codes <- read_csv("dataRaw/FAOSTAT_countrycodes.csv") 
+
+yld_pot <- read_dta("data_intermediary/yld_potential_rJcYdFg.dta")
+
+################################
+#'## Prepare data
+################################
+
+# clean codes
+FAO_codes_c <- FAO_codes %>% 
+  rename_all(~str_replace_all(., " ", "_"))%>% 
+  rename(iso2=`ISO2_Code`,
+         iso3_check=`ISO3_Code`,
+         countrycode=Country_Code) %>%  
+  mutate(iso2=str_to_lower(iso2))
+
+## clean/merge ylds
+yld_smooth_c <- yld_smooth %>% 
+  select(-production, -country_grp, -prop_global_area,
+         -area_hat, -area) %>% 
+  prj_clean_crop_names() %>% 
+  left_join(FAO_codes_c %>% 
+              select(countrycode, starts_with("iso")), by = "countrycode") %>% 
+  left_join(yld_pot %>% 
+              select(iso3, crop, yield_pot), by = c("iso3", "crop"))
+
+yld_smooth_c
+
+## CHECK: missing YP?
+yld_smooth_c %>% 
+  filter(is.na(yield_pot)) %>% 
+  count(country, countrycode, iso3, crop) %>% 
+  spread(crop, n, fill = 0)
+
+
+yld_pot %>% 
+  filter(str_detect(country, "China|ivo|Taiwan|Lao|Korea"))
+
+################################
+#'## Further cleaning
+################################
+
+
+## clean further
+yld_smooth_c %>%  
+  filter(iso3!= iso3_check) %>% 
+  mat_check_0row()
+
+yld_smooth_c2 <- yld_smooth_c %>% 
+  select(-iso3_check, -iso2) %>% 
+  relocate(iso3, .after = country)
+
+yld_smooth_c2
+
+## CHECK: balanced? OK!
+yld_smooth_c2 %>% 
+  filter(!is.na(yield)) %>% 
+  count(countrycode, crop, name = "n_years") %>% 
+  count(n_years) %>% 
+  filter(n_years!=56) %>% 
+  mat_check_0row(message_ok = "OK, 56 obs for all crop-country pairs present")
+
+max(yld_smooth_c2$year)
+
+################################
+#'## Export
+################################
+
+write_csv(yld_smooth_c2, "dataAnalysis/Ylds_raw_hat_pot_2016_rWsDaQp.rds")
+## READ AS: ylds_final <- read_csv("dataAnalysis/Ylds_raw_hat_pot_2016_rWsDaQp.rds")
+# rWsDaQp
